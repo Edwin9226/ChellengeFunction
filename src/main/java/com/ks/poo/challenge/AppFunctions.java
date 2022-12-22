f839e35aa01e2cf04900dca9d045524d3d6ff22e
Edwin Pinchao <miguepinchao1992@gmail.com>
2022-12-22 06:36:43 -0600

    Uso de la interfaz

--- src/main/java/com/ks/poo/challenge/AppFunctions.java
+++ src/main/java/com/ks/poo/challenge/AppFunctions.java
@@ -6,8 +6,20 @@ public class AppFunctions {
 
     public static  void main(String[] args){
 
-        Function<A,String> 
+        Function<A,String> func1= x-> x.toString();
                 A a= new A(20.1, "prueba", 68);
+
+                String apply= func1.apply(a);
+                System.out.println(apply);
+
+        Function<B,String> func2= y-> y.toString();
+        B b= new B(12.5, "prueba", "68");
+
+        String applyb = func2.apply(b);
+        System.out.println(applyb);
+
+
+
     }
 
     }
